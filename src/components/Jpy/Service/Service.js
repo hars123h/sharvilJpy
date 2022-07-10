f9a45d0f95ea723b6b969db86a1092fc0b77b43d
Pravin Kumar <2030111@sliet.ac.in>
2022-07-10 23:16:39 +0530

    minor changes

--- src/components/Jpy/Service/Service.js
+++ src/components/Jpy/Service/Service.js
@@ -21,14 +21,14 @@ const Service = () => {
           <div data-aos="slide-right" className="jpy_serviceCard1">
             <h2>IT DEVELOPMENT</h2>
             <div className="jpy_serviceCard__para">
-              <p>システム開発全般を経験豊富なエンジニアチームを駆使して、実行することが可能です。 AIからDX化のための複雑なシステムまで, さらに最新のWeb3関連の開発も可能です。 ヒアリングをベースに、お客様に最適な解決策をシステムで実現することができます。</p>
+              <p>システム開発全般を経験豊富なエンジニアチ<br />ームを駆使して、実行することが可能です。<br /> AIからDX化のための複雑なシステムまで, <br /> さらに最新のWeb3関連の開発も可能です。<br /> ヒアリングをベースに、お客様に最適な解決 <br /> 策 をシステムで実現することができます。</p>
             </div>
           </div>
           <div data-aos="fade-up" className="jpy_serviceCard2__center">
             <div className="jpy_serviceCard2">
               <h2>BUSINESS CONSULTING</h2>
               <div className="jpy_serviceCard__para">
-                <p>0から事業を作り上げるところから、既存の事業をさらに広い観点から成長させるところまで幅広くサポートさせていただくことができます。すでに多くの領域で事業展開をサポートしてきたコンサルタントが併走し、お客様の事業のさらなる成長をサポートします。</p>
+                <p>0から事業を作り上げるところから、既存の<br /> 事業をさらに広い観点から成長させるところ <br /> まで幅広くサポートさせていただくことがで <br /> きます。すでに多くの領域で事業展開をサポ <br /> ートしてきたコンサルタントが併走し、お客 <br /> 様の事業のさらなる成長をサポートします。</p>
               </div>
             </div>
           </div>
@@ -36,7 +36,7 @@ const Service = () => {
             <div className="jpy_serviceCard3">
               <h2>GLOBAL BUSINESS</h2>
               <div className="jpy_serviceCard__para">
-                <p>0から事業を作り上げるところから、既存の事業をさらに広い観点から成長させるところまで幅広くサポートさせていただくことができます。すでに多くの領域で事業展開をサポートしてきたコンサルタントが併走し、お客様の事業のさらなる成長をサポートします。</p>
+                <p>弊社の世界中に広がる強いネットワークを活用し、御 社の既存事業を世界各地に広げることをサポートさせ ていただきます。UAEからEU,またはアジア地域での さらなる顧客獲得をお手伝いいたします。言語に関し ても、全く問題なく世界中お客様の事業を広げます。</p>
               </div>
             </div>
           </div>
@@ -91,7 +91,7 @@ const Service = () => {
           <div className="service__webpageDesigne__rightBox">
             <div className={fade}>
               <h3>WEBPAGE DESIGN</h3>
-              <p className='service__webpageDesigne__rightBox__datashow' data-aos="fade-up" >{WebData}</p>
+              <p  className='service__webpageDesigne__rightBox__datashow' data-aos="fade-up" >{WebData}</p>
             </div>
           </div>
         </div>
