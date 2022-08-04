562ad6136bef56ba96cc852bd78026f4d69c449a
Pravin Kumar <2030111@sliet.ac.in>
2022-08-05 02:27:06 +0530

    css changes

--- src/components/Jpy/Service/Service2.js
+++ src/components/Jpy/Service/Service2.js
@@ -9,11 +9,11 @@ function Service2() {
     const [Blockchain, setBlockchain] = useState('dataHidden')
     const [application, setApplication] = useState('dataHidden')
     const [webthree, setWebthree] = useState('dataHidden')
-    const webaPageData = ()=>{
+    const webaPageData = () => {
         setWebpage('datashow');
         setReactNative('dataHidden')
     }
-    const reactNativeData = ()=>{
+    const reactNativeData = () => {
         setWebpage('dataHidden');
         setReactNative('datashow')
     }
@@ -22,14 +22,49 @@ function Service2() {
 
             <div className="jpy_serviceWrapper2">
                 <div className="jpy_serviceWrapper2__cards">
-                    <div data-aos="fade-up" className="jpy_serviceWrapper2__card">
+                    <table >
+
+                        <tr className='NumberRow' data-aos="fade-up">
+                            <td className="jpy_serviceWrapper2__card__circle">
+                                <p>01</p>
+                            </td>
+                            <td className="jpy_serviceWrapper2__card__circle">
+                                <p>02</p>
+                            </td>
+                            <td className="jpy_serviceWrapper2__card__circle">
+                                <p>03</p>
+                            </td>
+                            <td className="jpy_serviceWrapper2__card__circle">
+                                <p>04</p>
+                            </td>
+                            <td className="jpy_serviceWrapper2__card__circle">
+                                <p>05</p>
+                            </td>
+                        </tr>
+                        <tr  data-aos="fade-up" className='tabletitle'>
+                            <td><h3 style={{color:"#fff"}}>ヒアリング</h3></td>
+                            <td><h3 style={{color:"#fff"}}>戦略策定</h3></td>
+                            <td><h3 style={{color:"#fff"}}>デザイン</h3></td>
+                            <td><h3 style={{color:"#fff"}}>開発・実行</h3></td>
+                            <td> <h3 style={{color:"#fff"}}>サポート</h3></td>
+                        </tr>
+                        <tr className='dataRow' data-aos="fade-up">
+                            <td style={{width:"170px"}}> <p style={{lineHeight: "2", letterSpacing: "2px",color:"#fff"}}>まずは、お客様の持っていらっしゃる課題をとにかく深掘りします。課題を明確にし、解決をサポートできる最適なチームをセットします。</p></td>
+                            <td style={{width:"170px"}}><p style={{lineHeight: "2", letterSpacing: "2px",color:"#fff"}}>発見し、明確に言語化した課題を解決するための戦略を策定していきます。もちろんですが、お客様と綿密に打ち合わせを重ねていきます。</p></td>
+                            <td style={{width:"170px"}} > <p style={{lineHeight: "2", letterSpacing: "2px",color:"#fff"}}>課題解決のために、システムにせよ、サービスにせよ、目に見えるデザインと、目に見えないデザインをどちらも究極まで詰め、仕上げます</p></td>
+                            <td style={{width:"170px"}}> <p style={{lineHeight: "2", letterSpacing: "2px",color:"#fff"}}>システムであれば、最適なチームで迅速にシステムを構築し、事業開発であれば最適なチームと最速で実行に移します。スピードが重要です。</p></td>
+                            <td style={{width:"170px"}}><p style={{lineHeight: "2", letterSpacing: "2px",color:"#fff"}}>商品やコンサルを納品して終わりではありません。無期限でお客様のサポートをさせていただきます。</p></td>
+                        </tr>
+
+                    </table>
+                    {/* <div className="jpy_serviceWrapper2__card">
                         <div className="jpy_serviceWrapper2__card__circle">
                             <p>01</p>
                         </div>
                         <h3>ヒアリング</h3>
                         <p >まずは、お客様の持っていらっしゃる課題をとにかく深掘りします。課題を明確にし、解決をサポートできる最適なチームをセットします。</p>
                     </div>
-                    <div data-aos="fade-up" className="jpy_serviceWrapper2__card">
+                    <div className="jpy_serviceWrapper2__card">
                         <div className="jpy_serviceWrapper2__card__circle">
                             <p>02</p>
                         </div>
@@ -44,28 +79,28 @@ function Service2() {
                         <p>課題解決のために、システムにせよ、サービスにせよ、目に見えるデザインと、目に見えないデザインをどちらも究極まで詰め、仕上げます</p>
                     </div>
                     <div data-aos="fade-up" className="jpy_serviceWrapper2__card ">
-                        <div className="jpy_serviceWrapper2__card__circle circle4">
+                        <div className="jpy_serviceWrapper2__card__circle">
                             <p>04</p>
                         </div>
                         <h3>開発・実行</h3>
                         <p>システムであれば、最適なチームで迅速にシステムを構築し、事業開発であれば最適なチームと最速で実行に移します。スピードが重要です。</p>
                     </div>
                     <div data-aos="fade-up" className="jpy_serviceWrapper2__card ">
-                        <div className="jpy_serviceWrapper2__card__circle circle5">
+                        <div className="jpy_serviceWrapper2__card__circle">
                             <p>05</p>
                         </div>
                         <h3>サポート</h3>
                         <p>商品やコンサルを納品して終わりではありません。無期限でお客様のサポートをさせていただきます。</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <div className="service__webpageDesigne">
                 <div className="service__webpageDesigne__left">
                     <p onClick={webaPageData} className='service__webpageDesigne__left__circle'><img src="images/jpy/web1.png" alt="" /></p>
                     <p onClick={reactNativeData} className='service__webpageDesigne__left__circle'><img src="images/jpy/web2.png" alt="" /></p>
-                    <p onClick={() =>  {setWebpage('dataHidden');setWebthree('dataHidden');setApplication('datashow');setBlockchain('dataHidden');setReactNative('dataHidden')}} className='service__webpageDesigne__left__circle'><img src="images/jpy/web3.png" alt="" /></p>
-                    <p onClick={() =>  {setWebpage('dataHidden');setWebthree('dataHidden');setApplication('dataHidden');setBlockchain('datashow');setReactNative('dataHidden')}} className='service__webpageDesigne__left__circle'><img src="images/jpy/web4.png" alt="" /></p>
-                    <p onClick={() =>  {setWebpage('dataHidden');setWebthree('datashow');setApplication('dataHidden');setBlockchain('dataHidden');setReactNative('dataHidden')}} className='service__webpageDesigne__left__circle'><img src="images/jpy/web5.png" alt="" /></p>
+                    <p onClick={() => { setWebpage('dataHidden'); setWebthree('dataHidden'); setApplication('datashow'); setBlockchain('dataHidden'); setReactNative('dataHidden') }} className='service__webpageDesigne__left__circle'><img src="images/jpy/web3.png" alt="" /></p>
+                    <p onClick={() => { setWebpage('dataHidden'); setWebthree('dataHidden'); setApplication('dataHidden'); setBlockchain('datashow'); setReactNative('dataHidden') }} className='service__webpageDesigne__left__circle'><img src="images/jpy/web4.png" alt="" /></p>
+                    <p onClick={() => { setWebpage('dataHidden'); setWebthree('datashow'); setApplication('dataHidden'); setBlockchain('dataHidden'); setReactNative('dataHidden') }} className='service__webpageDesigne__left__circle'><img src="images/jpy/web5.png" alt="" /></p>
                 </div>
                 <div className="service__webpageDesigne__rightBox">
 
